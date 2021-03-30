6705d1aecca5594fab04c528d668c204dbbb5864
billshu <bill.shu@gravitysupplychain.com>
2021-03-30 20:10:19 +0800

    Minor tweaks

--- TopAct.Domain/Entities/Contact.cs
+++ TopAct.Domain/Entities/Contact.cs
@@ -1,9 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TopAct.Domain.Entities
 {
     public class Contact : Entity, IAggregateRoot
     {
+        private Contact(ContactId id,
+            string firstName,
+            string lastName,
+            string organisationName,
+            string websiteUrl,
+            string notes,
+            IList<Phone> phones,
+            IList<Address> addresses,
+            IList<Email> emails,
+            IList<Category> categories,
+            IList<Tag> tags,
+            IList<CustomField> customFields)
+        {
+            Id = id ?? throw new ArgumentNullException(nameof(id));
+            FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
+            LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
+            OrganisationName = organisationName;
+            WebsiteUrl = websiteUrl;
+            Notes = notes;
+            Phones = phones ?? new List<Phone>();
+            Addresses = addresses ?? new List<Address>();
+            Emails = emails ?? new List<Email>();
+            Categories = categories ?? new List<Category>();
+            Tags = tags ?? new List<Tag>();
+            CustomFields = customFields ?? new List<CustomField>();
+        }
+
         public ContactId Id { get; private set; }
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
@@ -19,5 +47,32 @@ namespace TopAct.Domain.Entities
         public IList<CustomField> CustomFields { get; private set; }
 
         public string FullName => $"{FirstName} {LastName}";
+
+        public static Contact CreateContact(string firstName,
+            string lastName,
+            string organisationName,
+            string websiteUrl,
+            string notes,
+            IList<Phone> phones,
+            IList<Address> addresses,
+            IList<Email> emails,
+            IList<Category> categories,
+            IList<Tag> tags,
+            IList<CustomField> customFields)
+        {
+            return new Contact(new ContactId(Guid.NewGuid()),
+                firstName,
+                lastName,
+                organisationName,
+                websiteUrl,
+                notes,
+                phones,
+                addresses,
+                emails,
+                categories,
+                tags,
+                customFields
+            );
+        }
     }
 }
--- TopAct.Domain/Entities/Email.cs
+++ TopAct.Domain/Entities/Email.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TopAct.Domain.Entities
+﻿namespace TopAct.Domain.Entities
 {
     public class Email : Entity
     {
