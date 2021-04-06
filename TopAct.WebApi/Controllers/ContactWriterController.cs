486c1e71c6f73ab47305eba8345e07829c3eea4f
billshu <bill.shu@gravitysupplychain.com>
2021-04-06 11:58:49 +0800

    Change namespace a bit

--- TopAct.WebApi/Controllers/ContactWriterController.cs
+++ TopAct.WebApi/Controllers/ContactWriterController.cs
@@ -5,7 +5,7 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using TopAct.Domain;
-using TopAct.Domain.Commands;
+using TopAct.Domain.Commanding;
 using TopAct.Domain.DtoModels;
 using static TopAct.WebApi.ControllerUtils;
 
